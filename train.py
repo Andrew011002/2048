2af6d1042972bca607f1877e1aca8c938492e8ea
andrew <andrewholmes011002@gmail.com>
2023-08-14 11:46:24 -0400

    updated train to use device and fixed some code for pathing

--- train.py
+++ train.py
@@ -1,70 +1,57 @@
+import torch
 import os
 import stable_baselines3 as sb3
 from env2 import Env2048
 
-# trains model over certain amount of time steps, logs after n time steps, does this N iterations
-def train(model, log_name, timesteps=10000, iters=100, save_directory=None):
+def train(model, timesteps=10000, iters=100, path=None, interval=1):
 
-    os.makedirs(save_directory, exist_ok=True)
-    path = os.path.join(save_directory, log_name)
+    dirname = os.path.dirname(path)
+    os.makedirs(dirname, exist_ok=True)
+    log_name = path.split("/")[-1]
 
     for i in range(iters):
-        model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=log_name)
+        model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=log_name, log_interval=interval)
         model.save(f"{path}-{i + 1}")
 
-# for retraining an already trained model
-def retrain(env, log_name, timesteps, iters):
-    name, steps = log_name.split('-') # get name of model, time steps
-    model = sb3.PPO.load(f'models/{log_name}') # load model
+def retrain(env, timesteps, iters, path):
+
+    dirname = os.path.dirname(path)
+    log_name = path.split("/")[-1]
+    name, steps = log_name.split('-') 
+    model = sb3.PPO.load(path) 
     model.set_env(env, force_reset=True)
 
-    # retrain the model (continuing from previous)
     for i in range(iters):
         model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=log_name)
-        model.save(f"models/{name}-{int(steps) + i + 1}")
+        model.save(f"{dirname}/{name}-{int(steps) + i + 1}")
 
 def simulate(model, env, episodes=1, verbose=False):
 
-    # create new sim for n episodes
     for ep in range(episodes):
         done = False
-        obs, info = env.reset() # init
+        obs, info = env.reset() 
 
-        # show info & env
         if verbose:
             print(f"Trial: {ep + 1}")
             env.render()
 
-        # run sim until the agent is done (fails or wins)
         while not done:
-
-            # predict based on env, then update env
             action, state = model.predict(obs)
             obs, reward, done, truncation, info = env.step(action.item())
 
-            # show info & env
             if verbose:
-                env.render() # show action made
+                env.render() 
                 print(f'Oberservation: {obs}')
-                print(f"Action: {action}\nReward: {reward}") # info
+                print(f"Action: {action}\nReward: {reward}") 
                 print(f'Info: {info}')
 
 def main():
-    # Enviornment
-    env = Env2048(size=4) # 4x4 2048 env
-
-    # TRAINING
-    # model = sb3.DQN("MlpPolicy", env, verbose=1, tensorboard_log="logs/") # Proximal Policy Optimization Algorithm
-    # train(model, log_name="Agent", timesteps=10000, iters=5000, save_directory="models") # 50 million runs in the game
-    
-    # RETRAINING
-    # ppo = sb3.PPO.load("models/ppo_4x4-100x10^4")
-    # retrain(env, "ppo-6824", timesteps=10000, iters=5000)
-
-    # SIMULATING
-    tag = 1
-    model = sb3.DQN.load(f"models/Agent-{tag}")
-    simulate(model, env, episodes=1, verbose=True)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    env = Env2048(size=4) 
+    log_directory = "logs/"
+    path = "models/model"
+    model = sb3.PPO("MlpPolicy", env, tensorboard_log=log_directory, device=device, verbose=1)
+    train(model, timesteps=10000, iters=5000, path=path, interval=5)
 
 if __name__ == "__main__":
     main()
