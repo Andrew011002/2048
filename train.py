d69ded92593192bc14d333cec7847ef1b24123bf
andrew <andrewholmes011002@gmail.com>
2023-08-14 16:50:59 -0400

    train updated to pass arguments, saving, loading, and simulating also works

--- train.py
+++ train.py
@@ -2,24 +2,21 @@ import torch
 import os
 import stable_baselines3 as sb3
 from env2 import Env2048
+from argparse import ArgumentParser
 
-def train(model, timesteps=10000, iters=100, path=None, interval=1):
+def train(model, timesteps=10000, iters=100,  save_path=None, log_name=None, log_interval=1):
 
-    dirname = os.path.dirname(path)
+    dirname = os.path.dirname(save_path)
     os.makedirs(dirname, exist_ok=True)
-    log_name = path.split("/")[-1]
 
     for i in range(iters):
-        model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=log_name, log_interval=interval)
-        model.save(f"{path}-{i + 1}")
+        model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=log_name, log_interval=log_interval)
+        model.save(f"{save_path}-{i + 1}")
 
-def retrain(env, timesteps, iters, path):
+def retrain(model, timesteps, iters, load_path, log_name):
 
-    dirname = os.path.dirname(path)
-    log_name = path.split("/")[-1]
-    name, steps = log_name.split('-') 
-    model = sb3.PPO.load(path) 
-    model.set_env(env, force_reset=True)
+    dirname = os.path.dirname(load_path)
+    name, steps = os.path.basename(load_path).replace(".zip", "").split("-")
 
     for i in range(iters):
         model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=log_name)
@@ -27,31 +24,54 @@ def retrain(env, timesteps, iters, path):
 
 def simulate(model, env, episodes=1, verbose=False):
 
-    for ep in range(episodes):
+    for episode in range(episodes):
         done = False
-        obs, info = env.reset() 
+        observation, info = env.reset() 
 
         if verbose:
-            print(f"Trial: {ep + 1}")
+            print(f"Trial: {episode + 1}")
             env.render()
 
         while not done:
-            action, state = model.predict(obs)
-            obs, reward, done, truncation, info = env.step(action.item())
+            action, state = model.predict(observation)
+            observation, reward, done, truncation, info = env.step(action.item())
 
             if verbose:
+                print(f"{'-' * 20}\nResults\n{'-' * 20}")
                 env.render() 
-                print(f'Oberservation: {obs}')
-                print(f"Action: {action}\nReward: {reward}") 
-                print(f'Info: {info}')
+                print(f"Oberservation: {list(observation)}\nAction: {action}\nReward: {reward}\nInfo: {info}") 
 
 def main():
+
+    parser = ArgumentParser(description="Determine whether to train, retrain, or simulate a model")
+    parser.add_argument("--path", type=str, help="Path for models (saving or loading).", default="models/model")
+    subpasrers = parser.add_subparsers(title="commands", dest="command")
+    subpasrers.add_parser(name="train", help="Command for training a model.")
+    subpasrers.add_parser(name="retrain", help="Command for retraining a model.")
+    subpasrers.add_parser(name="simulate", help="Command for simulating a model.")
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     env = Env2048(size=4) 
-    log_directory = "logs/"
-    path = "models/model"
-    model = sb3.PPO("MlpPolicy", env, tensorboard_log=log_directory, device=device, verbose=1)
-    train(model, timesteps=10000, iters=5000, path=path, interval=5)
+    timesteps = 10000
+    iterations = 5000
+    log_intervals = 5
+    args = parser.parse_args()
+    path = args.path
+
+    if args.command == "train":
+        log_name = "logs"
+        model = sb3.DQN("MlpPolicy", env, tensorboard_log=log_name, device=device, verbose=1)
+        train(model, timesteps=timesteps, iters=iterations, save_path=path, log_name=log_name, log_interval=log_intervals)
+
+    elif args.command == "retrain":
+        log_name = "logs"
+        model = sb3.DQN.load(path, env, device=device, print_system_info=True)
+        retrain(model, timesteps=timesteps, iters=iterations, log_name=log_name, load_path=path)
+
+    elif args.command == "simulate":
+        episodes = 1
+        model = sb3.DQN.load(path, env, device=device, print_system_info=True)
+        simulate(model, env, episodes, verbose=1)
 
 if __name__ == "__main__":
     main()
